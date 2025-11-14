1d6b26e96a589127891348497214ae0f12b33b28
fabricio <fabriciosantosc11@gmail.com>
2025-11-14 19:21:17 -0300

    atualizei modulo 3

---
+++ 3_CORE_MODULES/2_retornando_html/index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const port = 3000
+
+const server = http.createServer((req,res) =>{
+ res.statusCode = 200
+ res.setHeader('Contenty-type', 'text/html')
+ res.end('<h1>Olá esse é meu primeiro servidor com html! </h1>')
+})
+
+server.listen(port, () =>{
+    console.log(`servidor rodando na porta ${port}`)
+})
