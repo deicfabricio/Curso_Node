83796f3bae7ec43241609d380bef61d9a15121cf
fabricio <fabriciosantosc11@gmail.com>
2025-11-17 23:55:36 -0300

    Create index.js

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const custompath = '/relatorios/fabricio/relatorio1.pdf'
+
+console.log(path.dirname(custompath))
+console.log(path.basename(custompath))
+console.log(path.extname(custompath))
