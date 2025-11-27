4093be3f415cceb2923e8137cbad8a0df14a9ddb
fabricio <fabriciosantosc11@gmail.com>
2025-11-27 18:46:12 -0300

    .

--- 5_PROJETO_ACCOUNTS/index.js
+++ 5_PROJETO_ACCOUNTS/index.js
@@ -1,4 +1,5 @@
 // npm install chalk@4.1.2
+// npm install inquirer@8.1.2
 
 // modulos externos 
 const inquirer = require("inquirer")
@@ -7,4 +8,18 @@ const chalk = require("chalk")
 
 // modulos internos
 const fs  = require("fs")
-console.log("INICIAMOS O PROJETO !")
+
+operation()
+
+function operation(){
+    inquirer.prompt([{
+        type:"list",
+        name: "action",
+        message: "O que voce deseja fazer ?",
+        choices: ['Criar Conta','Consultar Saldo','Depositar','Sacar','Sair']
+    }
+    ]).then((answer) => {
+        const action = answer['action']
+        console.log(action)
+    }).catch((err) => console.log(err))
+}
