6202540d231f44aa654618daf316fb2cea09884c
fabricio <fabriciosantosc11@gmail.com>
2025-12-05 16:13:02 -0300

    estrutura de repeticao handlebars

---
+++ 7_TEMPLATE_ENGINE/6_estrutura_repeticao/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req,res) => {
+    const items = ["item a", "item b", "item c"]
+
+    res.render('dashboard', {items})
+})
+
+app.get('/', (req,res) => {
+    const user = {
+        name: 'Fabricio',
+        surname: 'Costa',
+        age: 18
+    }
+
+    const palavra = 'teste'
+
+    const auth = false
+
+    const approved = false
+
+    res.render('home', {user: user, palavra, auth, approved})
+})
+
+app.listen(3000, () => {
+    console.log("App rodando...");
+    
+})
