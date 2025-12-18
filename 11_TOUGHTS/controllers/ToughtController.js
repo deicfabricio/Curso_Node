5f150ccb55db6fc07ed2f3f456dd326da7fa264e
fabricio <fabriciosantosc11@gmail.com>
2025-12-18 09:48:57 -0300

    dashboard botao de edit

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -8,10 +8,72 @@ module.exports = class ToughtController {
     }
 
     static async dashboard(req,res) {
-        res.render('toughts/dashboard')
+        const userId = req.session.userid
+
+        const user = await User.findOne({where: { id:userId },include: Tought, plain: true })
+
+        //check if user exists
+        if(!user) {
+            res.redirect('/login')
+        }
+
+        const toughts = user.Toughts.map((result) => result.dataValues)
+
+        let emptyToughts = false
+
+        if(toughts.length === 0) {
+            emptyToughts = true
+        }
+
+        res.render('toughts/dashboard', {toughts})
     }
 
     static createTought(req,res) {
         res.render('toughts/create')
+    }
+
+    static async createToughtSave(req,res) {
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid 
+        }
+
+        try {
+            await Tought.create(tought)
+
+            req.flash('message', 'Pensamento criado com sucesso! ')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log('Aconteceu um erro: ' + error)
+        }
+    }
+
+    static async removeTought(req,res) {
+
+        const id = req.body.id
+        const UserId = req.session.userid
+
+       try {
+         await Tought.destroy({where: {id: id, UserId: UserId}})
+
+        req.flash('message', 'Pensamento removido com sucesso! ')
+
+        req.session.save(() => {
+         res.redirect('/toughts/dashboard')
+          })
+       } catch (error) {
+        console.log('Aconteceu um erro: ' + error)
+       }
+    }
+
+    static async updateTought(req,res) {
+        const id = req.params.id
+
+        const tought = await Tought.findOne({where: {id: id},raw: true})
+
+        res.render('toughts/edit', {tought})
     }
 }
